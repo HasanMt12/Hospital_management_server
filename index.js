1a6737df3d8adcc9ba626ddc10b87f7984e32b77
Hasan Mt <hasan.2mahmud22@gmail.com>
2022-12-20 17:14:44 +0600

    added

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
 
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gniuvqv.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
   try {
-    await client.connect();
-  
+    const doctorsDb = client.db('hospitalManagement').collection('doctors')
+
+    app.get('/doctors' , async(req,res) => {
+        const query = {};
+        const allDoctors = await doctorsDb.find(query).toArray();
+        res.send(allDoctors);
+    })
+       
     
     
 
