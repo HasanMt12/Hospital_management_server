432bb59fa1491bf2b4a5dcabf56c0b7eb165fefd
Hasan Mt <hasan.2mahmud22@gmail.com>
2022-12-29 20:23:56 +0600

    admin role doctor role nurse role funcionality added

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -5,30 +5,49 @@ const {
 } = require("../collections/collections");
 // demo code
 
-router.get("/", async (req, res) => {
-  try {
-    res.send(" users route is okay");
 
-   
-  } catch (error) {
-    res.send({ error: error.message });
-  }
-});
 
 router.post("/", async (req, res) => {
   try {
      const user = req.body;
-  console.log(user);
+  // console.log(user);
   const result = await usersCollection.insertOne(user);
   res.send(result);
-
-
   } catch (error) {
     res.send({
       error: error.message
     });
   }
 });
+
+    //get all users by database and send data to client side
+    router.get('/', async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    })
+
+    router.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+     router.get('/doctors/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isDoctor: user?.role === 'doctor'});
+        });
+
+    router.get('/nurse/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isNurse: user?.role === 'nurse'});
+        });
+
 
 
 
